b326a6b0b55b43668f2c1a4101e2f4faafb1c1cb
Paul Chen <paulchen408@gmail.com>
2015-07-21 12:22:51 -0700

    make mods

--- follow_bot.py
+++ follow_bot.py
@@ -1,2 +1,3 @@
-from twitter_follow_bot import auto_follow_list
-auto_follow_list()
+from twitter_follow_bot import auto_follow
+
+auto_follow('keyword', count=100) #follows up to 100 users who recently tweeted keyword
--- retweet_fav_bot.py
+++ retweet_fav_bot.py
@@ -3,10 +3,10 @@ from twitter_follow_bot import auto_fav, auto_rt
 
 # run 15 times a day (once per hour) starting 6AM PST, total of 3*1*15= 45 favorites a day
 for x in range(0, 2):
-    auto_fav('entrepreneurship', count=1) #add your own keywords
-    auto_rt('entrepreneurship', count=1, result_type="recent")
+    auto_fav('keyword1', count=1) #add your own keywords
+    auto_rt('keyword1', count=1, result_type="recent")
     time.sleep(60)
-    auto_fav('user testing', count=1) #add your own keywords
-    auto_rt('user testing', count=1, result_type="recent")
+    auto_fav('keyword2', count=1) #add your own keywords
+    auto_rt('keyword2', count=1, result_type="recent")
     time.sleep(60)
     #add more if necessary
